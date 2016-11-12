77443beb63a6227fc57824cbbb8ad6cf07e023dc
Szymon Sędek <szymon.sedek@gmail.com>
2016-11-12 16:35:10 +0100

    Add files via upload

---
+++ BubbleSort/bubbleSort_spec.rb
@@ -0,0 +1,71 @@
+require 'simplecov'
+SimpleCov.start
+
+require './algo/bubbleSort'
+
+RSpec.describe 'bubbleSort.rb' do
+
+  describe '#bubble' do
+
+    it 'should be defined method bubbleSort' do
+      expect { bubbleSort([7,6,5,9,2]) }.not_to raise_error
+    end
+    it 'swap empty array' do
+      expect(swap([], 0, 0)).to match_array([nil])
+    end
+    it 'swap elements in array' do
+      expect(swap([1, 2], 0, 1)).to match_array([2,1])
+    end
+    it 'get correct result' do
+      expect(bubbleSort([1, 2, 1])).to match_array([1,1,2])
+    end
+    it 'SortEmptyList' do
+      expect(bubbleSort([])).to match_array([])
+    end
+    it 'sort one element' do
+      expect(bubbleSort([1])).to match_array([1])
+    end
+    it 'sort two elements in correct order' do
+      expect(bubbleSort([1,2])).to match_array([1,2])
+    end
+    it 'sort two elements in reverse order' do
+      expect(bubbleSort([2,1])).to match_array([1,2])
+    end
+    it 'sort number random' do
+      expect(bubbleSort([6, 7, 2, 4, 1, 5, 8, 3])).to match_array([1,2,3,4,5,6,7,8])
+    end
+    it 'sorting a list with duplicate elements' do
+      expect(bubbleSort([1,2,1])).to match_array([1,1,2])
+    end
+    it 'sort one letter' do
+      expect(bubbleSort(['a'])).to match_array(['a'])
+    end
+    it 'sort one negativ number' do
+      expect(bubbleSort([-1])).to match_array([-1])
+    end
+    it 'sort few negativ number' do
+      expect(bubbleSort([-1,-2,-1])).to match_array([-2,-1,-1])
+    end
+    it 'sort mix number' do
+      expect(bubbleSort([6, -7, 2, 4, -1, -5, 8, 3])).to match_array([-7,-5,-1,2,3,4,6,8])
+    end
+    it 'sort two letter in correct order' do
+      expect(bubbleSort(['a','b'])).to match_array(['a', 'b'])
+    end
+    it 'sort two lettter in reverse order' do
+      expect(bubbleSort(['b', 'a'])).to match_array(['a','b'])
+    end
+    it 'sort letter random' do
+      expect(bubbleSort(['f','g','b','d','a','e','h','c'])).to match_array(['a','b','c','d','e','f','g','h'])
+    end
+    it 'sort letter and number as string' do
+      expect(bubbleSort(['a','1','-7','2'])).to match_array(['-7','1','2','a'])
+    end
+    it 'sort nil array' do
+      expect(bubbleSort([nil])).to match_array([nil])
+    end
+
+
+  end
+
+end
